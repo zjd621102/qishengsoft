cc84ad27edcbf16e563c2af17921a3337ee6ee93
zhoujd <zjd621102@gmail.com>
2012-12-17 19:52:51 +0800

    zhoujd分支初始化

--- src/test/java/com/yecoo/Test.java
+++ src/test/java/com/yecoo/Test.java
@@ -1,15 +1,42 @@
 package com.yecoo;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Properties;
+
+import com.yecoo.util.StrUtils;
 
 public class Test {
 	
+	public String getUrl() {
+		String url = this.getClass().getResource("/").getPath() + "../classes/message.properties";
+		String sqliteUrl = "";
+		InputStream in;
+		System.out.println(url);
+		try {
+			in = new BufferedInputStream(new FileInputStream(url));
+			Properties p = new Properties();
+			p.load(in);
+			sqliteUrl = p.getProperty("sqliteUrl");
+			in.close();
+		} catch (Exception e) {
+			StrUtils.WriteLog(this.getClass().getName() + ".getSqliteUrl()", e);
+		}
+		return sqliteUrl;
+	}
+	
 	public static void main(String[] args) throws Exception {
+		Test test = new Test();
+		String sqliteUrl = test.getUrl();
+		
 		Class.forName("org.sqlite.JDBC");
-		Connection connection = DriverManager.getConnection("jdbc:sqlite:src/main/webapp/resources/erpsoft.s3db");
+		Connection connection = DriverManager.getConnection(sqliteUrl);
+		System.out.println(sqliteUrl);
 		Statement statement = connection.createStatement();
 		ResultSet resultSet = statement.executeQuery("PRAGMA table_info('SUSER')");
 //		while (resultSet.next()) {
