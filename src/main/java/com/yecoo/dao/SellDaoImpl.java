780f73fcfb36fcbaa0d72b6216721d06341a0e21
zhoujd <zjd621102@163.com>
2015-12-28 08:43:31 +0800

    修改生成采购单

--- src/main/java/com/yecoo/dao/SellDaoImpl.java
+++ src/main/java/com/yecoo/dao/SellDaoImpl.java
@@ -310,7 +310,7 @@ public class SellDaoImpl extends BaseDaoImpl {
 		if(iReturn >= 1) { //保存行项表
 			sql = "INSERT INTO bbuyrow"
 				+ " SELECT NULL, '" + buyid + "', n.materialid, n.materialname, n.unit, n.price, m.num, 1,"
-				+ " n.price * m.num sum, o.manuid, o.manuname, o.manucontact, o.manutel, NULL FROM ("
+				+ " n.price * m.num sum, o.manuid, o.manuname, o.manucontact, o.manutel, NULL, NULL FROM ("
 				+ "SELECT a.materialid, SUM(b.materialnum * c.num) num"
 				+ " FROM smaterial a, sproductrow b, bsellrow c WHERE a.materialid = b.materialid"
 				+ " AND b.productid = c.productid AND c.sellid = '" + form.getValue("sellid") + "' GROUP BY a.materialid"
