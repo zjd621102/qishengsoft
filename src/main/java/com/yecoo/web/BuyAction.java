5d9cd699c59b5d5035334e559ae6c19738023762
zhoujd <zjd621102@163.com>
2016-02-19 11:32:06 +0800

    修改采购单不关闭页面

--- src/main/java/com/yecoo/web/BuyAction.java
+++ src/main/java/com/yecoo/web/BuyAction.java
@@ -139,7 +139,7 @@ public class BuyAction {
 		AjaxObject ajaxObject = null;
 		int iReturn = buyDaoImpl.ediBuy(form, request);
 		if (iReturn >= 0) {
-			ajaxObject = new AjaxObject("修改成功！", "buy_list", "closeCurrent");
+			ajaxObject = new AjaxObject("修改成功！", "buy_list", "");
 
 			StrUtils.saveLog(request, "修改采购单", form);
 		} else {
